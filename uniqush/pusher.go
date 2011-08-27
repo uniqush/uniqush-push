e476fca4adab5d66e3e26cdeb6fb33f7208a467d
Monnand <monnand@gmail.com>
2011-08-27 02:14:07 -0400

    added the errors for pusher

--- uniqush/pusher.go
+++ uniqush/pusher.go
@@ -17,3 +17,59 @@ func (p PushErrorIncompatibleOS) String() string {
     return p.s.String() + " does not compatible with " + p.o.String()
 }
 
+type remoteServerError struct {
+    msg string
+}
+
+func (r remoteServerError) String() string {
+    return r.msg
+}
+
+type QuotaExceededError struct {
+    remoteServerError
+}
+
+func NewQuotaExceededError() QuotaExceededError {
+    return QuotaExceededError{remoteServerError{"Service Quota Exceeded"}}
+}
+
+type DeviceQuotaExceededError struct {
+    remoteServerError
+}
+
+func NewDeviceQuotaExceededError() DeviceQuotaExceededError {
+    return DeviceQuotaExceededError{remoteServerError{"Device Quota Exceeded"}}
+}
+
+type UnregisteredError struct {
+    remoteServerError
+}
+
+func NewUnregisteredError() UnregisteredError {
+    return UnregisteredError{remoteServerError{"Device Unsubcribed"}}
+}
+
+type MessageTooBigError struct {
+    remoteServerError
+}
+
+func NewMessageTooBigError() MessageTooBigError {
+    return MessageTooBigError{remoteServerError{"Message Too Big"}}
+}
+
+type InvalidSubscriberError struct {
+    remoteServerError
+}
+
+func NewInvalidSubscriberError() InvalidSubscriberError {
+    return InvalidSubscriberError{remoteServerError{"Invalid Subscriber"}}
+}
+
+type InvalidServiceProviderError struct {
+    remoteServerError
+}
+
+func NewInvalidServiceProviderError() InvalidServiceProviderError {
+    return InvalidServiceProviderError{remoteServerError{"Inalid Service Provider"}}
+}
+
