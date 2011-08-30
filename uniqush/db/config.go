96f49db0790eb19df51b2323098d203971d7ed1a
Monnand <monnand@gmail.com>
2011-08-30 01:57:34 -0400

    Database config

---
+++ uniqush/db/config.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+    "os"
+    //"bufio"
+    "io/ioutil"
+    "json"
+    "fmt"
+)
+
+type DatabaseConfig struct {
+    Engine string
+    Name string
+    User string
+    Password string
+    Host string
+    Port int
+}
+
+const (
+    DEFAULT_DBCONF_FILE string = "/etc/uniqush.conf"
+)
+
+func LoadDatabaseConfig(filename string) (conf *DatabaseConfig, err os.Error) {
+    if len(filename) == 0 {
+        filename = DEFAULT_DBCONF_FILE
+    }
+    content, e := ioutil.ReadFile(filename)
+    if e != nil {
+        err = e
+        conf = nil
+        return
+    }
+    /*
+    str := string(content)
+    fmt.Print(str, "\n")
+    */
+    conf = new(DatabaseConfig)
+    err = json.Unmarshal(content, conf)
+    return
+}
+
+func (c *DatabaseConfig) String() string {
+    ret := fmt.Sprintf("engine: %v;\nname: %v;\nuser: %v;\npassowrd: %v;\nhost: %v\nport: %d\n",
+                       c.Engine, c.Name, c.User, c.Password, c.Host, c.Port)
+
+    return ret
+}
+
---
+++ uniqush/db/config_test.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+    "testing"
+    "fmt"
+)
+
+func TestDatabaseConfig(t *testing.T) {
+    fmt.Print("Start database config test...\n")
+    conf, err := LoadDatabaseConfig("")
+    if err != nil {
+        t.Errorf("Error: %v\n", err)
+    }
+    fmt.Print(conf, "\n")
+    fmt.Print("OK\n")
+}
