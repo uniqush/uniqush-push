b76b9e25049c930f4b54c173f89664f0c996a6b5
Monnand <monnand@gmail.com>
2011-09-05 07:33:17 -0400

    timer. need to be tested

--- uniqush/c2dmpusher_test.go
+++ uniqush/c2dmpusher_test.go
@@ -27,12 +27,10 @@ import (
 )
 
 
-const (
-    registration_id string = "APA91bHSvnhxU_VDRTnNh2m1TqZUBGC9rvOdb71Zz2aD4VX0gALsNXkd2N0G9nrUgvDtnERdxNi3WNyFJXSFr_OlaJYqyLENLAzw8zOxFv_yvNg2kjwdQJ0"
-    auth string = "DQAAALkAAAAeqF537EgIS48gts0EiLBZN8GCzoPuBgfWhBYHCErbLQcLRtaKKqn9mSjjXbyBL8K6cZ1TFnMtg5fsXaIOTic1D262M9revomDaeHeOdt-rXA4ps7Dg98PkZWuWEPxT-n3K_n466llh14l6dMLaUZTNxY3tXTBsSIYJc2Fa7LkogNUNrESGwFKgKY6R0ZjjiuNCDQRm8lLt6nnMmXygiKsr5yykD4Npi15TG9d3CqdoWAS54xHoSNsojajs7Wv4ng"
-)
-
 func TestC2DMPush(t *testing.T) {
+    /*
+    var registration_id string = "APA91bHSvnhxU_VDRTnNh2m1TqZUBGC9rvOdb71Zz2aD4VX0gALsNXkd2N0G9nrUgvDtnERdxNi3WNyFJXSFr_OlaJYqyLENLAzw8zOxFv_yvNg2kjwdQJ0"
+    var auth string = "DQAAALkAAAAeqF537EgIS48gts0EiLBZN8GCzoPuBgfWhBYHCErbLQcLRtaKKqn9mSjjXbyBL8K6cZ1TFnMtg5fsXaIOTic1D262M9revomDaeHeOdt-rXA4ps7Dg98PkZWuWEPxT-n3K_n466llh14l6dMLaUZTNxY3tXTBsSIYJc2Fa7LkogNUNrESGwFKgKY6R0ZjjiuNCDQRm8lLt6nnMmXygiKsr5yykD4Npi15TG9d3CqdoWAS54xHoSNsojajs7Wv4ng"
     sp := NewC2DMServiceProvider("provider", "monnand@gmail.com", auth)
     s := NewAndroidDeliveryPoint("sub", "nan.deng.osu@gmail.com", registration_id)
     data := make(map[string]string, 10)
@@ -44,5 +42,6 @@ func TestC2DMPush(t *testing.T) {
         return
     }
     t.Log("Successfully send notification, id=", id, "\n")
+    */
 }
 
---
+++ uniqush/timer.go
@@ -0,0 +1,107 @@
+/*
+ *  Uniqush by Nan Deng
+ *  Copyright (C) 2010 Nan Deng
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Nan Deng <monnand@gmail.com>
+ *
+ */
+
+package uniqush
+
+import (
+    "github.com/petar/GoLLRB/llrb"
+    "time"
+)
+
+type TimerActionProcessor interface {
+    Act(time int64, data interface{})
+}
+
+type Timer struct {
+    tree *llrb.Tree
+    actionProcessor TimerActionProcessor
+    ch <-chan *TimerAction
+    waitTime int64
+}
+
+type TimerAction struct {
+    execTime int64
+    data interface{}
+}
+
+const (
+    MAX_TIME int64 = 0x0FFFFFFFFFFFFFFF
+)
+
+func NewTimerAction(execTime int64, data interface{}) *TimerAction {
+    ret := new(TimerAction)
+    ret.execTime = execTime
+    ret.data = data
+    return ret
+}
+
+func actionLess(a, b interface{}) bool{
+    return a.(*TimerAction).execTime < a.(*TimerAction).execTime
+}
+
+func NewTimer(p TimerActionProcessor, ch <-chan *TimerAction) *Timer {
+    ret := new(Timer)
+    ret.tree = llrb.New(actionLess)
+    ret.actionProcessor = p
+    ret.ch = ch
+    ret.waitTime = MAX_TIME
+    return ret
+}
+
+func (t *Timer) Run() {
+    for {
+        select {
+        case action := <-t.ch:
+            t.tree.ReplaceOrInsert(action)
+            if action.execTime <= time.Nanoseconds() + 10 {
+                t.actionProcessor.Act(time.Nanoseconds(), action.data)
+                continue
+            }
+            x := t.tree.Min()
+            if x == nil {
+                t.waitTime = MAX_TIME
+                continue
+            }
+            action = x.(*TimerAction)
+            t.waitTime = action.execTime - time.Nanoseconds()
+        case <-time.After(t.waitTime):
+            x := t.tree.Min()
+            if x == nil {
+                t.waitTime = MAX_TIME
+                continue
+            }
+            action := x.(*TimerAction)
+            for action.execTime <= time.Nanoseconds() {
+                t.actionProcessor.Act(time.Nanoseconds(), action.data)
+                t.tree.DeleteMin()
+                x := t.tree.Min()
+                if x == nil {
+                    t.waitTime = MAX_TIME
+                    continue
+                }
+                action = x.(*TimerAction)
+            }
+            t.waitTime = action.execTime - time.Nanoseconds()
+        }
+    }
+}
+
