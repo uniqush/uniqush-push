d1d024e86f3c4dbbabccdaf514589f7e59b7df3e
Monnand <monnand@gmail.com>
2011-09-07 01:20:18 -0400

    added the flag

--- uniqush.go
+++ uniqush.go
@@ -26,11 +26,15 @@ import (
     "uniqush"
     "os"
     "fmt"
+    "flag"
 )
 
+var conf = flag.String("config", "/etc/uniqush/uniqush.conf", "Config file path")
+
 func main() {
-    conf := "/etc/uniqush/uniqush.conf"
-    unisys, err := uniqush.LoadUniqushSystem(conf)
+    //conf := "/etc/uniqush/uniqush.conf"
+    flag.Parse()
+    unisys, err := uniqush.LoadUniqushSystem(*conf)
     if err != nil {
         fmt.Fprintf(os.Stderr, "Fatal Error: %v\n", err)
         os.Exit(-1)
