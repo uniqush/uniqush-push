85d6e00a8db91da6300438b0d50250a53e66c70b
Monnand <monnand@gmail.com>
2011-09-14 19:38:27 -0400

    Changed the license from LGPL to Apache 2.0. Go is static compiled!

--- uniqush/c2dmpusher.go
+++ uniqush/c2dmpusher.go
@@ -1,35 +1,19 @@
 /*
- *  Uniqush by Nan Deng
- *  Copyright (C) 2010 Nan Deng
+ * Copyright 2011 Nan Deng
  *
- *  This software is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3.0 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this software; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Nan Deng <monnand@gmail.com>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-
-package uniqush
-
-import (
-	"os"
-	"http"
-	"strings"
-	"io/ioutil"
-	"url"
-	//    "url"
-)
 
 /* FIXME
  * Yes, it is http not https
