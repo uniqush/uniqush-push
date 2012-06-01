a57dafd7f79a5d45117d58c1d8758c416ba20023
Monnand <monnand@gmail.com>
2012-06-01 05:48:30 -0400

    tested cache. need more test

--- cache/cache.go
+++ cache/cache.go
@@ -21,6 +21,7 @@ import (
 	"time"
 	"sync"
 	"container/list"
+	"fmt"
 )
 
 type Flusher interface {
@@ -35,26 +36,30 @@ type dirtyElement struct {
 	value interface{}
 }
 
+type cacheItem struct {
+	key string
+	value interface{}
+}
+
 type Cache struct {
 	mu sync.Mutex
 	flushPeriod time.Duration
 	data map[string]*list.Element
 	dirtyList *list.List
 	list *list.List
-	capacity uint32
-	size uint32
+	capacity int
 	flusher Flusher
 	maxNrDirty int
 }
 
-func (c *Cache) flush() {
+func (c *Cache) Flush() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.flusher == nil {
 		return
 	}
 	for e := c.dirtyList.Front(); e != nil; e = e.Next() {
-		if de, ok := e.Value.(dirtyElement); ok {
+		if de, ok := e.Value.(*dirtyElement); ok {
 			if de.removed {
 				c.flusher.Remove(de.key)
 			} else if de.modified {
@@ -65,17 +70,34 @@ func (c *Cache) flush() {
 	c.dirtyList = list.New()
 }
 
+func (c *Cache) debug() {
+	fmt.Printf("nr elems %v <= %v, nr dirty elems %v < %v\n",
+			   c.list.Len(), c.capacity, c.dirtyList.Len(), c.maxNrDirty)
+	fmt.Println("-----------------elements------------")
+	for e := c.list.Front(); e != nil; e = e.Next() {
+		item := e.Value.(*cacheItem)
+		fmt.Printf("%v: %v\n", item.key, item.value)
+	}
+	fmt.Println("-----------dirty elements------------")
+	for e := c.dirtyList.Front(); e != nil; e = e.Next() {
+		de := e.Value.(*dirtyElement)
+		fmt.Printf("%v: %v; modified: %v; removed %v\n",
+				   de.key, de.value, de.modified, de.removed)
+	}
+	fmt.Println("-------------------------------------")
+}
+
 func (c *Cache) checkAndFlush() {
 	c.mu.Lock()
 	if c.maxNrDirty > 0 && c.dirtyList.Len() >= c.maxNrDirty {
 		c.mu.Unlock()
-		c.flush()
+		c.Flush()
 	} else {
 		c.mu.Unlock()
 	}
 }
 
-func New(capacity uint32, maxNrDirty int, flushPeriod time.Duration, flusher Flusher) {
+func New(capacity int, maxNrDirty int, flushPeriod time.Duration, flusher Flusher) *Cache {
 	cache := new(Cache)
 
 	cache.flushPeriod = flushPeriod
@@ -89,18 +111,19 @@ func New(capacity uint32, maxNrDirty int, flushPeriod time.Duration, flusher Flu
 	if flushPeriod.Seconds() > 0.9 {
 		go func () {
 			time.Sleep(flushPeriod)
-			cache.flush()
+			cache.Flush()
 		}()
 	}
+	return cache
 }
 
 func (c *Cache) Get(key string) interface{} {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if elem, ok := c.data[key]; ok {
-		value := elem.Value
+		item := elem.Value.(*cacheItem)
 		c.list.MoveToFront(elem)
-		return value
+		return item.value
 	}
 	return nil
 }
@@ -110,23 +133,28 @@ func (c *Cache) Set(key string, value interface{}) {
 	defer c.checkAndFlush()
 	defer c.mu.Unlock()
 
-	if len(c.data) > int(c.capacity) {
-		return
-	}
 	de := &dirtyElement {
 		modified: true,
 		removed: false,
 		key: key,
 		value: value,
 	}
+
 	if e, ok := c.data[key]; ok {
-		e.Value = value
+		item := e.Value.(*cacheItem)
+		item.value = value
 		c.list.MoveToFront(e)
 		c.dirtyList.PushBack(de)
 	} else {
-		elem := c.list.PushFront(value)
+		elem := c.list.PushFront(&cacheItem{key:key, value:value})
 		c.data[key] = elem
 		c.dirtyList.PushBack(de)
+		if len(c.data) > c.capacity {
+			last := c.list.Back()
+			item := last.Value.(*cacheItem)
+			c.list.Remove(last)
+			delete(c.data, item.key)
+		}
 	}
 	return
 }
@@ -144,7 +172,8 @@ func (c *Cache) Delete(key string) interface{} {
 	c.dirtyList.PushBack(de)
 	if elem, ok := c.data[key]; ok {
 		delete(c.data, key)
-		return elem.Value
+		item := elem.Value.(*cacheItem)
+		return item.value
 	}
 	return nil
 }
