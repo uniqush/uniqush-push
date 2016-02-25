uniqush/uniqush-push

commit b11f9a19a37daf53ee8366b57136ed02ba236c83

put everything under same repo

push/errors.go
@@ -1,0 +1,172 @@
+/*
+ * Copyright 2011 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package push
+
+import (
+	"fmt"
+	"time"
+)
+
+/*********************/
+
+type RetryError struct {
+	After time.Duration
+}
+
+func (e *RetryError) Error() string {
+	return fmt.Sprintf("Retry")
+}
+
+func NewRetryError(after time.Duration) error {
+	return &RetryError{After: after}
+}
+
+/*********************/
+
+type PushServiceProviderUpdate struct {
+	Provider *PushServiceProvider
+}
+
+func (e *PushServiceProviderUpdate) Error() string {
+	return fmt.Sprintf("PushServiceProvider=%v Update", e.Provider.Name())
+}
+
+func NewPushServiceProviderUpdate(psp *PushServiceProvider) error {
+	return &PushServiceProviderUpdate{Provider: psp}
+}
+
+/*********************/
+
+type DeliveryPointUpdate struct {
+	Destination *DeliveryPoint
+}
+
+func (e *DeliveryPointUpdate) Error() string {
+	return fmt.Sprintf("DeliveryPoint=%v Update", e.Destination.Name())
+}
+
+func NewDeliveryPointUpdate(dp *DeliveryPoint) error {
+	return &DeliveryPointUpdate{Destination: dp}
+}
+
+/*********************/
+
+type IncompatibleError struct {
+}
+
+func (e *IncompatibleError) Error() string {
+	return fmt.Sprintf("Incompatible")
+}
+
+func NewIncompatibleError() error {
+	return &IncompatibleError{}
+}
+
+/*********************/
+
+type BadDeliveryPoint struct {
+	Destination *DeliveryPoint
+	Details string
+}
+
+func (e *BadDeliveryPoint) Error() string {
+	if e.Details != "" {
+		return fmt.Sprintf("BadDeliveryPoint %v: %v", e.Destination.Name(), e.Details)
+	}
+	return fmt.Sprintf("BadDeliveryPoint %v", e.Destination.Name())
+}
+
+func NewBadDeliveryPoint(dp *DeliveryPoint) error {
+	return &BadDeliveryPoint{Destination: dp, Details: ""}
+}
+
+func NewBadDeliveryPointWithDetails(dp *DeliveryPoint, details string) error {
+	return &BadDeliveryPoint{Destination: dp, Details: details}
+}
+
+/*********************/
+
+type BadPushServiceProvider struct {
+	Provider *PushServiceProvider
+	Details string
+}
+
+func (e *BadPushServiceProvider) Error() string {
+	if e.Details != "" {
+		return fmt.Sprintf("BadPushServiceProvider %v: %v", e.Provider.Name(), e.Details)
+	}
+	return fmt.Sprintf("BadPushServiceProvider %v", e.Provider.Name())
+}
+
+func NewBadPushServiceProvider(psp *PushServiceProvider) error {
+	return &BadPushServiceProvider{Provider: psp, Details: ""}
+}
+
+func NewBadPushServiceProviderWithDetails(psp *PushServiceProvider, details string) error {
+	return &BadPushServiceProvider{Provider: psp, Details: details}
+}
+
+/*********************/
+
+type BadNotification struct {
+	Details string
+}
+
+func (e *BadNotification) Error() string {
+	if e.Details != "" {
+		return fmt.Sprintf("Bad Notification: %s", e.Details)
+	}
+	return fmt.Sprintf("Bad Notification")
+}
+
+func NewBadNotification() error {
+	return &BadNotification{""}
+}
+
+func NewBadNotificationWithDetails(details string) error {
+	return &BadNotification{details}
+}
+
+/*********************/
+
+type UnsubscribeUpdate struct {
+	Destination *DeliveryPoint
+}
+
+func (e *UnsubscribeUpdate) Error() string {
+	return fmt.Sprintf("RequestUnsubscribe %v", e.Destination.Name())
+}
+
+func NewUnsubscribeUpdate(dp *DeliveryPoint) error {
+	return &UnsubscribeUpdate{Destination: dp}
+}
+
+/*********************/
+
+type ConnectionError struct {
+	Err error
+}
+
+func (e *ConnectionError) Error() string {
+	return fmt.Sprintf("ConnectionError %v", e)
+}
+
+func NewConnectionError(err error) error {
+	return &ConnectionError{Err: err}
+}
+

db/dbconfig.go
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2011 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package db
+
+import (
+	"fmt"
+	. "github.com/uniqush/uniqush-push/push"
+)
+
+type DatabaseConfig struct {
+	Engine    string
+	Name      string
+	User      string
+	Password  string
+	Host      string
+	Port      int
+	CacheSize int
+
+	/* dump the dirty data to db every EverySec seconds,
+	 * if there are more than LeastDirty dirty items
+	 */
+	EverySec   int64
+	LeastDirty int
+
+	PushServiceManager *PushServiceManager
+}
+
+/*
+const (
+    DEFAULT_DBCONF_FILE string = "/etc/uniqush.conf"
+)
+
+func LoadDatabaseConfig(filename string) (conf *DatabaseConfig, err os.Error) {
+    if len(filename) == 0 {
+        filename = DEFAULT_DBCONF_FILE
+    }
+    content, e := ioutil.ReadFile(filename)
+    if e != nil {
+        err = e
+        conf = nil
+        return
+    }
+    conf = new(DatabaseConfig)
+    err = json.Unmarshal(content, conf)
+    return
+}
+*/
+
+func (c *DatabaseConfig) String() string {
+	ret := fmt.Sprintf("engine: %v;\nname: %v;\nuser: %v;\npassowrd: %v;\nhost: %v\nport: %d\n",
+		c.Engine, c.Name, c.User, c.Password, c.Host, c.Port)
+
+	return ret
+}

db/pushdb.go
@@ -1,0 +1,262 @@
+/*
+ * Copyright 2011 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package db
+
+import (
+	"errors"
+	"fmt"
+	. "github.com/uniqush/uniqush-push/push"
+	"sync"
+)
+
+type PushServiceProviderDeliveryPointPair struct {
+	PushServiceProvider *PushServiceProvider
+	DeliveryPoint       *DeliveryPoint
+}
+
+// You may always want to use a front desk to get data from db
+type PushDatabase interface {
+
+	// The push service provider may by anonymous whose Name is empty string
+	// For anonymous push service provider, it will be added to database
+	// and its Name will be set
+	RemovePushServiceProviderFromService(service string, push_service_provider *PushServiceProvider) error
+
+	// The push service provider may by anonymous whose Name is empty string
+	// For anonymous push service provider, it will be added to database
+	// and its Name will be set
+	AddPushServiceProviderToService(service string,
+		push_service_provider *PushServiceProvider) error
+
+	ModifyPushServiceProvider(psp *PushServiceProvider) error
+
+	// The delivery point may be anonymous whose Name is empty string
+	// For anonymous delivery point, it will be added to database and its Name will be set
+	// Return value: selected push service provider, error
+	AddDeliveryPointToService(service string,
+		subscriber string,
+		delivery_point *DeliveryPoint) (*PushServiceProvider, error)
+
+	// The delivery point may be anonymous whose Name is empty string
+	// For anonymous delivery point, it will be added to database and its Name will be set
+	// Return value: selected push service provider, error
+	RemoveDeliveryPointFromService(service string,
+		subscriber string,
+		delivery_point *DeliveryPoint) error
+
+	ModifyDeliveryPoint(dp *DeliveryPoint) error
+
+	GetPushServiceProviderDeliveryPointPairs(service string,
+		subscriber string) ([]PushServiceProviderDeliveryPointPair, error)
+
+	FlushCache() error
+}
+
+type pushDatabaseOpts struct {
+	db pushRawDatabase
+	/* TODO Fine grained locks */
+	dblock sync.RWMutex
+}
+
+/*
+func NewPushDatabaseOpts(conf *DatabaseConfig) (PushDatabase, error) {
+	var err error
+	f := new(pushDatabaseOpts)
+	udb, err := newPushRedisDB(conf)
+	if udb == nil || err != nil {
+		return nil, err
+	}
+	f.db = NewCachedUniqushDatabase(udb, udb, conf)
+	if f.db == nil {
+		return nil, errors.New("Cannot create cached database")
+	}
+	return f, nil
+}
+*/
+
+func NewPushDatabaseWithoutCache(conf *DatabaseConfig) (PushDatabase, error) {
+	var err error
+	f := new(pushDatabaseOpts)
+	f.db, err = newPushRedisDB(conf)
+	if f.db == nil || err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func (f *pushDatabaseOpts) FlushCache() error {
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	return f.db.FlushCache()
+}
+
+func (f *pushDatabaseOpts) RemovePushServiceProviderFromService(service string, push_service_provider *PushServiceProvider) error {
+	name := push_service_provider.Name()
+	if name == "" {
+		return errors.New("InvalidPushServiceProvider")
+	}
+	db := f.db
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	return db.RemovePushServiceProviderFromService(service, name)
+}
+
+func (f *pushDatabaseOpts) AddPushServiceProviderToService(service string,
+	push_service_provider *PushServiceProvider) error {
+	if push_service_provider == nil {
+		return nil
+	}
+	name := push_service_provider.Name()
+	if len(name) == 0 {
+		return errors.New("InvalidPushServiceProvider")
+	}
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	e := f.db.SetPushServiceProvider(push_service_provider)
+	if e != nil {
+		return e
+	}
+	return f.db.AddPushServiceProviderToService(service, push_service_provider.Name())
+}
+
+func (f *pushDatabaseOpts) AddDeliveryPointToService(service string,
+	subscriber string,
+	delivery_point *DeliveryPoint) (*PushServiceProvider, error) {
+	if delivery_point == nil {
+		return nil, nil
+	}
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	pspnames, err := f.db.GetPushServiceProvidersByService(service)
+	if err != nil {
+		return nil, err
+	}
+	if pspnames == nil {
+		return nil, errors.New(fmt.Sprintf("Cannot Find Service %s", service))
+	}
+	if len(delivery_point.Name()) == 0 {
+		return nil, errors.New("InvalidDeliveryPoint")
+	}
+
+	for _, pspname := range pspnames {
+		psp, e := f.db.GetPushServiceProvider(pspname)
+		if e != nil {
+			return nil, e
+		}
+		if psp == nil {
+			continue
+		}
+		if psp.PushServiceName() == delivery_point.PushServiceName() {
+			err = f.db.SetDeliveryPoint(delivery_point)
+			if err != nil {
+				return nil, err
+			}
+			err = f.db.AddDeliveryPointToServiceSubscriber(service, subscriber, delivery_point.Name())
+			if err != nil {
+				return nil, err
+			}
+			err = f.db.SetPushServiceProviderOfServiceDeliveryPoint(service, delivery_point.Name(), psp.Name())
+			if err != nil {
+				return nil, err
+			}
+			return psp, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("Cannot Find Push Service Provider with Type %s", delivery_point.PushServiceName()))
+}
+
+func (f *pushDatabaseOpts) RemoveDeliveryPointFromService(service string,
+	subscriber string,
+	delivery_point *DeliveryPoint) error {
+	if delivery_point.Name() == "" {
+		return errors.New("InvalidDeliveryPoint")
+	}
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	err := f.db.RemoveDeliveryPointFromServiceSubscriber(service, subscriber, delivery_point.Name())
+	if err != nil {
+		return err
+	}
+	err = f.db.RemovePushServiceProviderOfServiceDeliveryPoint(service, delivery_point.Name())
+	return err
+}
+
+func (f *pushDatabaseOpts) GetPushServiceProviderDeliveryPointPairs(service string,
+	subscriber string) ([]PushServiceProviderDeliveryPointPair, error) {
+	f.dblock.RLock()
+	defer f.dblock.RUnlock()
+	dpnames, err := f.db.GetDeliveryPointsNameByServiceSubscriber(service, subscriber)
+	if err != nil {
+		return nil, err
+	}
+	if dpnames == nil {
+		return nil, nil
+	}
+	ret := make([]PushServiceProviderDeliveryPointPair, 0, len(dpnames))
+
+	for srv, dpl := range dpnames {
+        for _, d := range dpl {
+            dp, e0 := f.db.GetDeliveryPoint(d)
+            if e0 != nil {
+                return nil, e0
+            }
+            if dp == nil {
+                continue
+            }
+
+            pspname, e := f.db.GetPushServiceProviderNameByServiceDeliveryPoint(srv, d)
+            if e != nil {
+                return nil, e
+            }
+
+            if len(pspname) == 0 {
+                continue
+            }
+
+            psp, e1 := f.db.GetPushServiceProvider(pspname)
+            if e1 != nil {
+                return nil, e1
+            }
+            if psp == nil {
+                continue
+            }
+
+            ret = append(ret, PushServiceProviderDeliveryPointPair{psp, dp})
+        }
+	}
+
+	return ret, nil
+}
+
+func (f *pushDatabaseOpts) ModifyPushServiceProvider(psp *PushServiceProvider) error {
+	if len(psp.Name()) == 0 {
+		return nil
+	}
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	return f.db.SetPushServiceProvider(psp)
+}
+
+func (f *pushDatabaseOpts) ModifyDeliveryPoint(dp *DeliveryPoint) error {
+	if len(dp.Name()) == 0 {
+		return nil
+	}
+	f.dblock.Lock()
+	defer f.dblock.Unlock()
+	return f.db.SetDeliveryPoint(dp)
+}

db/pushdb_test.go
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2011 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package db
+
+import (
+	"testing"
+	"fmt"
+	"strconv"
+	redis "github.com/monnand/goredis"
+	. "github.com/uniqush/uniqush-push/push"
+)
+
+var dbconf *DatabaseConfig
+
+func init() {
+	dbconf = new(DatabaseConfig)
+	dbconf.Host = ""
+	dbconf.Port = 0
+	dbconf.Name = "0"
+	dbconf.Engine = "redis"
+}
+
+func connectDatabase() (db PushDatabase, err error) {
+	db, err = NewPushDatabaseWithoutCache(dbconf)
+	return
+}
+
+func clearData() {
+	var client redis.Client
+	c := dbconf
+	if c.Host == "" {
+		c.Host = "localhost"
+	}
+	if c.Port <= 0 {
+		c.Port = 6379
+	}
+	if c.Name == "" {
+		c.Name = "0"
+	}
+	client.Addr = fmt.Sprintf("%s:%d", c.Host, c.Port)
+	client.Password = c.Password
+	var err error
+	client.Db, err = strconv.Atoi(c.Name)
+	if err != nil {
+		client.Db = 0
+	}
+	client.Flush(true)
+}
+
+
+func TestConnectAndDelete(t *testing.T) {
+	_, err := connectDatabase()
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	clearData()
+}
+
+func TestInsertAndGetPushServiceProviders(t *testing.T) {
+}
+

db/pushrawdbcache.go
@@ -1,0 +1,152 @@
+/*
+ * Copyright 2012 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package db
+
+import (
+	"github.com/uniqush/cache"
+	. "github.com/uniqush/uniqush-push/push"
+	"time"
+)
+
+type pushRawDatabaseCache struct {
+	pspCache  *cache.Cache
+	dpCache   *cache.Cache
+	srvSub2Dp *cache.Cache
+	srv2Psp   *cache.Cache
+	dbwriter  pushRawDatabaseWriter
+	dbreader  pushRawDatabaseReader
+}
+
+type pspFlusher struct {
+	cdb *pushRawDatabaseCache
+}
+
+func (self *pspFlusher) Add(key string, value interface{}) {
+	if psp, ok := value.(*PushServiceProvider); ok {
+		self.cdb.dbwriter.SetPushServiceProvider(psp)
+	}
+}
+
+func (self *pspFlusher) Remove(key string) {
+	self.cdb.dbwriter.RemovePushServiceProvider(key)
+}
+
+type dpFlusher struct {
+	cdb *pushRawDatabaseCache
+}
+
+func (self *dpFlusher) Add(key string, value interface{}) {
+	if psp, ok := value.(*DeliveryPoint); ok {
+		self.cdb.dbwriter.SetDeliveryPoint(psp)
+	}
+}
+
+func (self *dpFlusher) Remove(key string) {
+	self.cdb.dbwriter.RemoveDeliveryPoint(key)
+}
+
+func NewpushRawDatabaseCache(c *DatabaseConfig,
+	dbreader pushRawDatabaseReader,
+	dbwriter pushRawDatabaseWriter) (*pushRawDatabaseCache, error) {
+	cacheSize := 1024
+	flushPeriod := 600
+	leastDirty := 128
+	if c != nil {
+		cacheSize = c.CacheSize
+		flushPeriod = int(c.EverySec)
+		leastDirty = c.LeastDirty
+	}
+
+	cdb := new(pushRawDatabaseCache)
+	cdb.dbreader = dbreader
+	cdb.dbwriter = dbwriter
+	pspflusher := &pspFlusher{cdb: cdb}
+	dpflusher := &dpFlusher{cdb: cdb}
+	cdb.pspCache = cache.New(cacheSize, leastDirty, time.Duration(flushPeriod)*time.Second, pspflusher)
+	cdb.dpCache = cache.New(cacheSize, leastDirty, time.Duration(flushPeriod)*time.Second, dpflusher)
+
+	// We will flush them manually
+	cdb.srvSub2Dp = cache.New(cacheSize, -1, time.Duration(0)*time.Second, nil)
+	cdb.srv2Psp = cache.New(cacheSize, -1, time.Duration(0)*time.Second, nil)
+	return cdb, nil
+}
+
+func (cdb *pushRawDatabaseCache) GetDeliveryPoint(dp string) (ret *DeliveryPoint, err error) {
+	if dpi := cdb.dpCache.Get(dp); dpi == nil {
+		ret, err = cdb.dbreader.GetDeliveryPoint(dp)
+		if err != nil {
+			return nil, err
+		}
+		cdb.dpCache.Set(dp, ret.Name())
+	} else {
+		ret = dpi.(*DeliveryPoint)
+	}
+	return ret, nil
+}
+
+func (cdb *pushRawDatabaseCache) GetPushServiceProvider(psp string) (ret *PushServiceProvider, err error) {
+	if pspi := cdb.pspCache.Get(psp); pspi == nil {
+		ret, err = cdb.dbreader.GetPushServiceProvider(psp)
+		if err != nil {
+			return nil, err
+		}
+		cdb.dpCache.Set(psp, ret.Name())
+	} else {
+		ret = pspi.(*PushServiceProvider)
+	}
+	return ret, nil
+}
+
+func (cdb *pushRawDatabaseCache) SetDeliveryPoint(dp *DeliveryPoint) error {
+	cdb.dpCache.Set(dp.Name(), dp)
+	return nil
+}
+
+func (cdb *pushRawDatabaseCache) SetPushServiceProvider(psp *PushServiceProvider) error {
+	cdb.pspCache.Set(psp.Name(), psp)
+	return nil
+}
+
+func (cdb *pushRawDatabaseCache) RemoveDeliveryPoint(dp string) error {
+	cdb.dpCache.Delete(dp)
+	return nil
+}
+
+func (cdb *pushRawDatabaseCache) RemovePushServiceProvider(psp string) error {
+	cdb.pspCache.Delete(psp)
+	return nil
+}
+
+func (cdb *pushRawDatabaseCache) GetDeliveryPointsNameByServiceSubscriber(srv, sub string) (dps map[string][]string, err error) {
+	key := srv + ":" + sub
+	if i := cdb.srvSub2Dp.Get(key); i == nil {
+		dps, err = cdb.dbreader.GetDeliveryPointsNameByServiceSubscriber(srv, sub)
+		if err != nil {
+			return nil, err
+		}
+		cdb.srvSub2Dp.Set(key, dps)
+	} else {
+		dps = i.(map[string][]string)
+	}
+	return dps, nil
+}
+
+/*
+func (cdb *pushRawDatabaseCache) AddDeliveryPointToServiceSubscriber(srv, sub, dp string) error {
+}
+*/

db/pushredisdb.go
@@ -1,0 +1,266 @@
+/*
+ * Copyright 2011 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package db
+
+import (
+	"errors"
+	"fmt"
+	redis "github.com/monnand/goredis"
+	. "github.com/uniqush/uniqush-push/push"
+	"strconv"
+	"strings"
+)
+
+type PushRedisDB struct {
+	client *redis.Client
+	psm    *PushServiceManager
+}
+
+const (
+	DELIVERY_POINT_PREFIX                                 string = "delivery.point:"
+	PUSH_SERVICE_PROVIDER_PREFIX                          string = "push.service.provider:"
+	SERVICE_SUBSCRIBER_TO_DELIVERY_POINTS_PREFIX          string = "srv.sub-2-dp:"
+	SERVICE_DELIVERY_POINT_TO_PUSH_SERVICE_RPVIDER_PREFIX string = "srv.dp-2-psp:"
+	SERVICE_TO_PUSH_SERVICE_PROVIDERS_PREFIX              string = "srv-2-psp:"
+	DELIVERY_POINT_COUNTER_PREFIX                         string = "delivery.point.counter:"
+)
+
+func newPushRedisDB(c *DatabaseConfig) (*PushRedisDB, error) {
+	if c == nil {
+		return nil, errors.New("Invalid Database Config")
+	}
+	if strings.ToLower(c.Engine) != "redis" {
+		return nil, errors.New("Unsupported Database Engine")
+	}
+	var client redis.Client
+	if c.Host == "" {
+		c.Host = "localhost"
+	}
+	if c.Port <= 0 {
+		c.Port = 6379
+	}
+	if c.Name == "" {
+		c.Name = "0"
+	}
+
+	client.Addr = fmt.Sprintf("%s:%d", c.Host, c.Port)
+	client.Password = c.Password
+	var err error
+	client.Db, err = strconv.Atoi(c.Name)
+	if err != nil {
+		client.Db = 0
+	}
+
+	ret := new(PushRedisDB)
+	ret.client = &client
+	ret.psm = c.PushServiceManager
+	if ret.psm == nil {
+		ret.psm = GetPushServiceManager()
+	}
+	return ret, nil
+}
+
+func (r *PushRedisDB) keyValueToDeliveryPoint(name string, value []byte) *DeliveryPoint {
+	psm := r.psm
+	dp, err := psm.BuildDeliveryPointFromBytes(value)
+	if err != nil {
+		return nil
+	}
+	return dp
+}
+
+func (r *PushRedisDB) keyValueToPushServiceProvider(name string, value []byte) *PushServiceProvider {
+	psm := r.psm
+	psp, err := psm.BuildPushServiceProviderFromBytes(value)
+	if err != nil {
+		return nil
+	}
+	return psp
+}
+
+func deliveryPointToValue(dp *DeliveryPoint) []byte {
+	return dp.Marshal()
+}
+
+func pushServiceProviderToValue(psp *PushServiceProvider) []byte {
+	return psp.Marshal()
+}
+
+func (r *PushRedisDB) GetDeliveryPoint(name string) (*DeliveryPoint, error) {
+	b, err := r.client.Get(DELIVERY_POINT_PREFIX + name)
+	if err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, nil
+	}
+	return r.keyValueToDeliveryPoint(name, b), nil
+}
+
+func (r *PushRedisDB) SetDeliveryPoint(dp *DeliveryPoint) error {
+	err := r.client.Set(DELIVERY_POINT_PREFIX+dp.Name(), deliveryPointToValue(dp))
+	return err
+}
+
+func (r *PushRedisDB) GetPushServiceProvider(name string) (*PushServiceProvider, error) {
+	b, err := r.client.Get(PUSH_SERVICE_PROVIDER_PREFIX + name)
+	if err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, nil
+	}
+	return r.keyValueToPushServiceProvider(name, b), nil
+}
+
+func (r *PushRedisDB) SetPushServiceProvider(psp *PushServiceProvider) error {
+	return r.client.Set(PUSH_SERVICE_PROVIDER_PREFIX+psp.Name(), pushServiceProviderToValue(psp))
+}
+
+func (r *PushRedisDB) RemoveDeliveryPoint(dp string) error {
+	_, err := r.client.Del(DELIVERY_POINT_PREFIX + dp)
+	return err
+}
+
+func (r *PushRedisDB) RemovePushServiceProvider(psp string) error {
+	_, err := r.client.Del(PUSH_SERVICE_PROVIDER_PREFIX + psp)
+	return err
+}
+
+func (r *PushRedisDB) GetDeliveryPointsNameByServiceSubscriber(srv, usr string) (map[string][]string, error) {
+    keys := make([]string, 1)
+    if !strings.Contains(usr, "*") && !strings.Contains(srv, "*") {
+        keys[0] = SERVICE_SUBSCRIBER_TO_DELIVERY_POINTS_PREFIX + srv + ":" + usr
+    } else {
+        var err error
+        keys, err = r.client.Keys(SERVICE_SUBSCRIBER_TO_DELIVERY_POINTS_PREFIX + srv + ":" + usr)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    ret := make(map[string][]string, len(keys))
+	for _, k := range keys {
+		m, err := r.client.Smembers(k)
+		if err != nil {
+			return nil, err
+		}
+		if m == nil {
+			continue
+		}
+        elem := strings.Split(k, ":")
+        s := elem[1]
+        if l, ok := ret[s]; !ok || l == nil {
+            ret[s] = make([]string, 0, len(keys))
+        }
+		for _, bm := range m {
+            dpl := ret[s]
+            dpl = append(dpl, string(bm))
+            ret[s] = dpl
+		}
+	}
+	return ret, nil
+}
+
+func (r *PushRedisDB) GetPushServiceProviderNameByServiceDeliveryPoint(srv, dp string) (string, error) {
+	b, err := r.client.Get(SERVICE_DELIVERY_POINT_TO_PUSH_SERVICE_RPVIDER_PREFIX + srv + ":" + dp)
+	if err != nil {
+		return "", err
+	}
+	if b == nil {
+		return "", nil
+	}
+	return string(b), nil
+}
+
+func (r *PushRedisDB) AddDeliveryPointToServiceSubscriber(srv, sub, dp string) error {
+	i, err := r.client.Sadd(SERVICE_SUBSCRIBER_TO_DELIVERY_POINTS_PREFIX+srv+":"+sub, []byte(dp))
+	if err != nil {
+		return err
+	}
+	if i == false {
+		return nil
+	}
+	_, err = r.client.Incr(DELIVERY_POINT_COUNTER_PREFIX + dp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *PushRedisDB) RemoveDeliveryPointFromServiceSubscriber(srv, sub, dp string) error {
+	j, err := r.client.Srem(SERVICE_SUBSCRIBER_TO_DELIVERY_POINTS_PREFIX+srv+":"+sub, []byte(dp))
+	if err != nil {
+		return err
+	}
+	if j == false {
+		return nil
+	}
+	i, e := r.client.Decr(DELIVERY_POINT_COUNTER_PREFIX + dp)
+	if e != nil {
+		return e
+	}
+	if i <= 0 {
+		_, e0 := r.client.Del(DELIVERY_POINT_COUNTER_PREFIX + dp)
+		if e0 != nil {
+			return e0
+		}
+		_, e1 := r.client.Del(DELIVERY_POINT_PREFIX + dp)
+		return e1
+	}
+	return e
+}
+
+func (r *PushRedisDB) SetPushServiceProviderOfServiceDeliveryPoint(srv, dp, psp string) error {
+	return r.client.Set(SERVICE_DELIVERY_POINT_TO_PUSH_SERVICE_RPVIDER_PREFIX+srv+":"+dp, []byte(psp))
+}
+
+func (r *PushRedisDB) RemovePushServiceProviderOfServiceDeliveryPoint(srv, dp string) error {
+	_, err := r.client.Del(SERVICE_DELIVERY_POINT_TO_PUSH_SERVICE_RPVIDER_PREFIX + srv + ":" + dp)
+	return err
+}
+
+func (r *PushRedisDB) GetPushServiceProvidersByService(srv string) ([]string, error) {
+	m, err := r.client.Smembers(SERVICE_TO_PUSH_SERVICE_PROVIDERS_PREFIX + srv)
+	if err != nil {
+		return nil, err
+	}
+	if m == nil {
+		return nil, nil
+	}
+	ret := make([]string, len(m))
+	for i, bm := range m {
+		ret[i] = string(bm)
+	}
+
+	return ret, nil
+}
+
+func (r *PushRedisDB) RemovePushServiceProviderFromService(srv, psp string) error {
+	_, err := r.client.Srem(SERVICE_TO_PUSH_SERVICE_PROVIDERS_PREFIX+srv, []byte(psp))
+	return err
+}
+
+func (r *PushRedisDB) AddPushServiceProviderToService(srv, psp string) error {
+	_, err := r.client.Sadd(SERVICE_TO_PUSH_SERVICE_PROVIDERS_PREFIX+srv, []byte(psp))
+	return err
+}
+
+func (r *PushRedisDB) FlushCache() error {
+	return r.client.Save()
+}

commit c73e25de05ab5081b5e25984149fd91334f28cd2

Fix stack overflow in errors
in Error(), fmt.Sprintf was calling Error(), of the same object.
fmt.Sprintf should be passed the member err.Err instead.

Fix bug introduced in b11f9a19

push/errors.go
@@ -276,7 +276,7 @@ type ConnectionError struct {
 }
 
 func (e *ConnectionError) Error() string {
-	return fmt.Sprintf("ConnectionError %v", e)
+	return fmt.Sprintf("ConnectionError %v", e.Err)
 }
 
 func NewConnectionError(err error) *ConnectionError {