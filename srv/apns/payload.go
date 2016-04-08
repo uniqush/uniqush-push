f318939e952967794f81d710e7522599aa7dae08
Tyson Andre <tandre@ifwe.co>
2016-04-07 17:31:06 -0700

    Allow providing raw payloads for APNS pushes, fix bug in content-available
    Should be cast to a number, instead of being a string.
    See https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/TheNotificationPayload.html
    
    > Provide this key with a value of 1 to indicate that new content is available

--- srv/apns/payload.go
+++ srv/apns/payload.go
@@ -19,13 +19,50 @@ package apns
 // Contains functions for building a payload from the url parameter abstraction.
 
 import (
+	"encoding/json"
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/uniqush/uniqush-push/push"
 	"github.com/uniqush/uniqush-push/srv/apns/common"
 )
 
+// validateRawAPNSPayload tests that the client-provided JSON payload can be sent to APNS.
+// It converts it to bytes if it is, otherwise it returns a push.PushError.
+func validateRawAPNSPayload(payload string) ([]byte, push.PushError) {
+	// https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW1
+	var data map[string]interface{}
+	err := json.Unmarshal([]byte(payload), &data)
+	if data == nil {
+		return nil, push.NewBadNotificationWithDetails(fmt.Sprintf("Could not parse payload: %v", err))
+	}
+	aps, ok := data["aps"]
+	if !ok {
+		return nil, push.NewBadNotificationWithDetails("Payload missing aps")
+	}
+	aps_dict, ok := aps.(map[string]interface{})
+	if !ok {
+		return nil, push.NewBadNotificationWithDetails("aps is not a dictionary")
+	}
+	if _, ok := aps_dict["alert"]; !ok {
+		if content_available, ok := aps_dict["content-available"]; !ok || content_available != "1" {
+			return nil, push.NewBadNotificationWithDetails("Missing alert and this is not a silent notification(content-available is not 1)")
+		}
+	}
+
+	// TODO: Could optionally validate provided fields further according to documentation of the "The Notification Payload" section.
+	// Creating a custom struct would make it simpler.
+	// (E.g. body, action-loc-key, loc-key, loc-args, badge, sound, content-available, launch-image)
+	return []byte(payload), nil
+}
+
 func toAPNSPayload(n *push.Notification) ([]byte, push.PushError) {
+	// If "uniqush.payload.apns" is provided, then that will be used instead of the other POST parameters.
+	if payloadJSON, ok := n.Data["uniqush.payload.apns"]; ok {
+		bytes, err := validateRawAPNSPayload(payloadJSON)
+		return bytes, err
+	}
 	payload := make(map[string]interface{})
 	aps := make(map[string]interface{})
 	alert := make(map[string]interface{})
@@ -39,17 +76,15 @@ func toAPNSPayload(n *push.Notification) ([]byte, push.PushError) {
 			alert[k] = v
 		case "loc-args":
 			alert[k] = parseList(v)
-		case "badge":
+		case "badge", "content-available":
 			b, err := strconv.Atoi(v)
 			if err != nil {
 				continue
 			} else {
-				aps["badge"] = b
+				aps[k] = b
 			}
 		case "sound":
 			aps["sound"] = v
-		case "content-available":
-			aps["content-available"] = v
 		case "img":
 			alert["launch-image"] = v
 		case "id":
@@ -59,6 +94,9 @@ func toAPNSPayload(n *push.Notification) ([]byte, push.PushError) {
 		case "ttl":
 			continue
 		default:
+			if strings.HasPrefix(k, "uniqush.") { // keys beginning with "uniqush." are reserved by uniqush.
+				continue
+			}
 			payload[k] = v
 		}
 	}
