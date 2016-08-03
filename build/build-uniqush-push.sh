85497870d29ec96f068ba6eb174ccfc8efafea7c
Tyson Andre <tandre@ifwe.co>
2016-08-02 17:00:32 -0700

    Update the installation script, for #128

--- build/build-uniqush-push.sh
+++ build/build-uniqush-push.sh
@@ -1,38 +1,44 @@
-#!/bin/sh
+#!/bin/bash -xeu
+# Depends on the ruby gem "fpm" being installed.
+# Exits immediately on error.
 
 TEMP=`pwd`/tmpgopath
 LICENSE=Apache-2.0
 
-mkdir -p $TEMP/bin
-mkdir -p $TEMP/src
-mkdir -p $TEMP/pkg
+mkdir -p "$TEMP/bin"
+mkdir -p "$TEMP/src"
+mkdir -p "$TEMP/pkg"
 
-GOBIN=$TEMP/bin GOPATH=$TEMP go get github.com/uniqush/uniqush-push
+if [ -d "$TEMP" ]; then
+	rm -rf "$TEMP"
+fi
 
-VERSION=`$TEMP/bin/uniqush-push --version | sed 's/uniqush-push //'`
+GOBIN="$TEMP/bin" GOPATH="$TEMP" go get github.com/uniqush/uniqush-push
 
-BUILD=`pwd`/uniqush-push-$VERSION
-mkdir -p $BUILD/usr/bin
-mkdir -p $BUILD/etc/uniqush/
+VERSION=`"$TEMP/bin/uniqush-push" --version | sed 's/uniqush-push //'`
 
-ARCH=`uname -m`
+BUILD=`pwd`/"uniqush-push-$VERSION"
+mkdir -p "$BUILD/usr/bin"
+mkdir -p "$BUILD/etc/uniqush/"
 
-cp $TEMP/bin/uniqush-push $BUILD/usr/bin
-cp $TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf $BUILD/etc/uniqush
-cp $TEMP/src/github.com/uniqush/uniqush-push/LICENSE $LICENSE
+ARCH="`uname -m`"
 
-fpm -s dir -t rpm -v $VERSION -n uniqush-push --license=$LICENSE --maintainer="Nan Deng" --vendor "uniqush" --url="http://uniqush.org" --category Network --description "Uniqush is a free and open source software which provides a unified push service for server-side notification to apps on mobile devices" -a $ARCH -C $BUILD .
+cp "$TEMP/bin/uniqush-push" "$BUILD/usr/bin"
+cp "$TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf" "$BUILD/etc/uniqush"
+cp "$TEMP/src/github.com/uniqush/uniqush-push/LICENSE" "$LICENSE"
 
-fpm -s dir -t deb -v $VERSION -n uniqush-push --license=$LICENSE --maintainer="Nan Deng" --vendor "uniqush" --url="http://uniqush.org" --category Network --description "Uniqush is a free and open source software which provides a unified push service for server-side notification to apps on mobile devices" -a $ARCH -C $BUILD .
+fpm -s dir -t rpm -v "$VERSION" -n uniqush-push --license="$LICENSE" --maintainer="Nan Deng" --vendor "uniqush" --url="http://uniqush.org" --category Network --description "Uniqush is a free and open source software which provides a unified push service for server-side notification to apps on mobile devices" -a "$ARCH" -C "$BUILD" .
 
-TARBALLNAME=uniqush-push_${VERSION}_$ARCH
-TARBALLDIR=`pwd`/$TARBALLNAME
-mkdir -p $TARBALLDIR
-cp $LICENSE $TARBALLDIR
-cp $TEMP/bin/uniqush-push $TARBALLDIR
-cp $TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf $TARBALLDIR/uniqush-push.conf
+fpm -s dir -t deb -v "$VERSION" -n uniqush-push --license="$LICENSE" --maintainer="Nan Deng" --vendor "uniqush" --url="http://uniqush.org" --category Network --description "Uniqush is a free and open source software which provides a unified push service for server-side notification to apps on mobile devices" -a "$ARCH" -C "$BUILD" .
 
-cat > $TARBALLNAME/install.sh << EOF
+TARBALLNAME="uniqush-push_${VERSION}_$ARCH"
+TARBALLDIR=`pwd`/"$TARBALLNAME"
+mkdir -p "$TARBALLDIR"
+cp "$LICENSE" "$TARBALLDIR"
+cp "$TEMP/bin/uniqush-push" "$TARBALLDIR"
+cp "$TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf" "$TARBALLDIR/uniqush-push.conf"
+
+cat > "$TARBALLNAME/install.sh" << EOF
 #!/bin/sh
 mkdir -p /etc/uniqush
 cp uniqush-push /usr/local/bin
@@ -40,13 +46,14 @@ cp uniqush-push.conf /etc/uniqush
 echo "Success!"
 EOF
 
-chmod +x $TARBALLDIR/install.sh
-tar czvf $TARBALLNAME.tar.gz $TARBALLNAME
+chmod +x "$TARBALLDIR/install.sh"
+tar czvf "$TARBALLNAME.tar.gz" "$TARBALLNAME"
 
-rm -rf $TEMP
-rm -rf $BUILD
-rm -rf $TARBALLDIR
+rm -rf "$TEMP"
+rm -rf "$BUILD"
+rm -rf "$TARBALLDIR"
 rm -f uniqush-push
 rm -f uniqush-push.conf
-rm -f $LICENSE 
+rm -f "$LICENSE"
 
+echo "Packages are found in uniqush-push_${VERSION}...rpm/.deb/.tar.gz"
