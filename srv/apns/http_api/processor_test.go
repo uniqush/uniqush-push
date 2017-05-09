c9dfecba37f823a8b38861a9e3e2d2bc68004dbb
Danny Andrianto <danny_andrianto@hotmail.com>
2017-05-09 18:05:09 +0700

    Add fail scenario

--- srv/apns/http_api/processor_test.go
+++ srv/apns/http_api/processor_test.go
@@ -35,6 +35,7 @@ var (
 	}
 	devToken = []byte("test_device_token")
 	payload  = []byte(`{"alert":"test_message"}`)
+	apiURL   = fmt.Sprintf("%s/3/device/%s", pushServiceProvider.VolatileData["addr"], hex.EncodeToString(devToken))
 )
 
 type MockJWTManager struct{}
@@ -43,7 +44,7 @@ func (*MockJWTManager) GenerateToken() (string, error) {
 	return authToken, nil
 }
 
-func TestAddRequest(t *testing.T) {
+func TestAddRequestPushSuccessful(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
@@ -58,7 +59,6 @@ func TestAddRequest(t *testing.T) {
 		ResChan:   resChan,
 	}
 
-	apiURL := fmt.Sprintf("%s/3/device/%s", request.PSP.VolatileData["addr"], hex.EncodeToString(devToken))
 	httpmock.RegisterResponder("POST", apiURL, func(r *http.Request) (*http.Response, error) {
 		// Return empty body
 		return httpmock.NewBytesResponse(http.StatusOK, nil), nil
@@ -69,6 +69,69 @@ func TestAddRequest(t *testing.T) {
 	for err := range errChan {
 		if err != nil {
 			t.Fatal("Error processing push request,", err)
+		}
+	}
+}
+
+func TestAddRequestPushFailConnectionError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	errChan := make(chan push.PushError)
+	resChan := make(chan *common.APNSResult, 1)
+
+	request := &common.PushRequest{
+		PSP:       pushServiceProvider,
+		Devtokens: [][]byte{devToken},
+		Payload:   payload,
+		ErrChan:   errChan,
+		ResChan:   resChan,
+	}
+
+	httpmock.RegisterResponder("POST", apiURL, func(r *http.Request) (*http.Response, error) {
+		return nil, fmt.Errorf("No connection")
+	})
+
+	NewRequestProcessor().AddRequest(request)
+
+	for err := range errChan {
+		switch err := err.(type) {
+		case *push.ConnectionError:
+		default:
+			t.Fatal("Expected Connection error, got", err)
+		}
+	}
+}
+
+func TestAddRequestPushFailNotificationError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	errChan := make(chan push.PushError)
+	resChan := make(chan *common.APNSResult, 1)
+
+	request := &common.PushRequest{
+		PSP:       pushServiceProvider,
+		Devtokens: [][]byte{devToken},
+		Payload:   payload,
+		ErrChan:   errChan,
+		ResChan:   resChan,
+	}
+
+	httpmock.RegisterResponder("POST", apiURL, func(r *http.Request) (*http.Response, error) {
+		response := &APNSErrorResponse{
+			Reason: "BadDeviceToken",
+		}
+		return httpmock.NewJsonResponse(http.StatusBadRequest, response)
+	})
+
+	NewRequestProcessor().AddRequest(request)
+
+	for err := range errChan {
+		switch err := err.(type) {
+		case *push.BadNotification:
+		default:
+			t.Fatal("Expected BadNotification error, got", err)
 		}
 	}
 }
