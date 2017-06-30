b5ca1f09d12d8c5e73c28bb63a4665067e050234
Tyson Andre <tandre@themeetgroup.com>
2017-06-29 17:00:02 -0700

    Update redis dependency from redis.v3 to redis.v5

--- db/pushredisdb.go
+++ db/pushredisdb.go
@@ -25,11 +25,11 @@ import (
 
 	"github.com/uniqush/log"
 	"github.com/uniqush/uniqush-push/push"
-	redis3 "gopkg.in/redis.v3"
+	redis5 "gopkg.in/redis.v5"
 )
 
 type PushRedisDB struct {
-	client *redis3.Client
+	client *redis5.Client
 	psm    *push.PushServiceManager
 }
 
@@ -67,10 +67,10 @@ func newPushRedisDB(c *DatabaseConfig) (*PushRedisDB, error) {
 	if err != nil {
 		db = 0
 	}
-	client := redis3.NewClient(&redis3.Options{
+	client := redis5.NewClient(&redis5.Options{
 		Addr:     fmt.Sprintf("%s:%d", c.Host, c.Port),
 		Password: c.Password,
-		DB:       db,
+		DB:       int(db),
 	})
 
 	ret := new(PushRedisDB)
@@ -419,7 +419,7 @@ func (r *PushRedisDB) RebuildServiceSet() error {
 		}
 		serviceNameSet[serviceName] = true
 	}
-	var serviceNameList []string
+	var serviceNameList []interface{}
 	for serviceName, _ := range serviceNameSet {
 		serviceNameList = append(serviceNameList, serviceName)
 	}
