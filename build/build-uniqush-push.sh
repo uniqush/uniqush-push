80610285d95a666bdb06985665db7b8d4a447c4a
Tyson Andre <tysonandre775@hotmail.com>
2019-11-22 17:42:10 -0500

    Update the build script to use `go mod`
    This will build the current master branch

--- build/build-uniqush-push.sh
+++ build/build-uniqush-push.sh
@@ -5,27 +5,29 @@
 TEMP=`pwd`/tmpgopath
 LICENSE=Apache-2.0
 
-mkdir -p "$TEMP/bin"
-mkdir -p "$TEMP/src"
-mkdir -p "$TEMP/pkg"
-
 if [ -d "$TEMP" ]; then
 	rm -rf "$TEMP"
 fi
+mkdir -p $TEMP
 
-GOBIN="$TEMP/bin" GOPATH="$TEMP" go get github.com/uniqush/uniqush-push
+pushd $TEMP
+git clone --depth 1 git@github.com:uniqush/uniqush-push.git
+pushd uniqush-push
+go get
+go build
+popd
+popd
 
-VERSION=`"$TEMP/bin/uniqush-push" --version | sed 's/uniqush-push //'`
-
+VERSION=`$TEMP/uniqush-push/uniqush-push --version | sed 's/uniqush-push //'`
 BUILD=`pwd`/"uniqush-push-$VERSION"
 mkdir -p "$BUILD/usr/bin"
 mkdir -p "$BUILD/etc/uniqush/"
 
 ARCH="`uname -m`"
 
-cp "$TEMP/bin/uniqush-push" "$BUILD/usr/bin"
-cp "$TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf" "$BUILD/etc/uniqush"
-cp "$TEMP/src/github.com/uniqush/uniqush-push/LICENSE" "$LICENSE"
+cp "$TEMP/uniqush-push/uniqush-push" "$BUILD/usr/bin"
+cp "$TEMP/uniqush-push/conf/uniqush-push.conf" "$BUILD/etc/uniqush"
+cp "$TEMP/uniqush-push/LICENSE" "$LICENSE"
 
 fpm -s dir -t rpm -v "$VERSION" -n uniqush-push --license="$LICENSE" --maintainer="Nan Deng" --vendor "uniqush" --url="http://uniqush.org" --category Network --description "Uniqush is a free and open source software which provides a unified push service for server-side notification to apps on mobile devices" -a "$ARCH" -C "$BUILD" .
 
@@ -35,8 +37,8 @@ TARBALLNAME="uniqush-push_${VERSION}_$ARCH"
 TARBALLDIR=`pwd`/"$TARBALLNAME"
 mkdir -p "$TARBALLDIR"
 cp "$LICENSE" "$TARBALLDIR"
-cp "$TEMP/bin/uniqush-push" "$TARBALLDIR"
-cp "$TEMP/src/github.com/uniqush/uniqush-push/conf/uniqush-push.conf" "$TARBALLDIR/uniqush-push.conf"
+cp "$TEMP/uniqush-push/uniqush-push" "$TARBALLDIR"
+cp "$TEMP/uniqush-push/conf/uniqush-push.conf" "$TARBALLDIR/uniqush-push.conf"
 
 cat > "$TARBALLNAME/install.sh" << EOF
 #!/bin/sh
