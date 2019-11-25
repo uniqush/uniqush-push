81ebbdb72292a5899f4a6f5f595b0132a7e48588
Tyson Andre <tysonandre775@hotmail.com>
2019-11-25 14:06:12 -0500

    Release uniqush-push 2.7.0
    Closes #252

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 var uniqushPushConfFlags = flag.String("config", "/etc/uniqush/uniqush-push.conf", "Config file path")
 var uniqushPushShowVersionFlag = flag.Bool("version", false, "Version info")
 
-var uniqushPushVersion = "uniqush-push 2.6.2-dev"
+var uniqushPushVersion = "uniqush-push 2.7.0"
 
 func installPushServices() {
 	srv.InstallGCM()
